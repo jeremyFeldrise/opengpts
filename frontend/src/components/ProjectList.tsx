0d904d72957609a26b51b4b70439ae5f7b199a6c
Thonio <anthonio.rj@hotmail.fr>
2025-05-18 17:25:28 +0300

    modal deletion

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -3,6 +3,10 @@ import { getProjects, selectProject, deleteProject } from '../api/projects'
 import { useNavigate } from 'react-router-dom'
 import { useConfigList } from '../hooks/useConfigList'
 import ProjectCard from './ProjectCard'
+import ModalComponent from './modalComponent'
+import { useState } from 'react'
+import { Trash, X } from 'lucide-react'
+import { Button } from './button'
 
 interface Project {
   project_id: string
@@ -11,6 +15,9 @@ interface Project {
 }
 
 export default function ProjectList() {
+  const [isOpenConfirm, setIsOpenConfirm] = useState(false)
+  const [deleteId, setDeleteId] = useState('')
+  const [isLoadingDeletion, setIsLoadingDeletion] = useState(false)
   const { data: projects, isLoading, isError, refetch } = useQuery<Project[]>('projects', getProjects)
   const { configs } = useConfigList();
 
@@ -30,14 +37,45 @@ export default function ProjectList() {
     return <ErrorCard />
   }
 
-  async function handleDelete(projectId: string) {
-    await deleteProject(projectId);
+  function handleDelete(projectId: string) {
+    setDeleteId(projectId)
+    setIsOpenConfirm(true)
   }
+
+  async function handleDeletion() {
+    setIsLoadingDeletion(true)
+    await deleteProject(deleteId).finally(() => setIsLoadingDeletion(false))
+  }
+
   return (
     <>
       {projects?.map((project) => (
         <ProjectCard key={project.project_id} project={project} chooseProject={chooseProject} handleDelete={handleDelete} refetch={refetch} />
       ))}
+      <ModalComponent title="Confirm Project Deletion" isOpen={isOpenConfirm} onClose={() => {
+        setIsOpenConfirm(false)
+        setDeleteId('')
+      }}>
+        <div className="">
+          <div className="text-sm mb-4 text-gray-400 font-light">Warning: This action cannot be undone. All intelligences, agents, and data associated with this project will be permanently deleted. Applications using this project will no longer be able to access its functionalities.</div>
+          <div className="grid grid-cols-2 gap-4">
+            <Button
+              variant="outline"
+              rightElem={<X />}
+              onClick={() => {
+                setIsOpenConfirm(false)
+                setDeleteId('')
+              }}>Cancel</Button>
+            <Button
+              disabled={isLoadingDeletion}
+              onClick={handleDeletion}
+              rightElem={<Trash />}
+            >
+              {isLoadingDeletion ? "Please wait..." : "Confirm"}
+            </Button>
+          </div>
+        </div>
+      </ModalComponent>
     </>
   )
 }
--- frontend/src/components/modalComponent.tsx
+++ frontend/src/components/modalComponent.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion"
 import { X } from "lucide-react"
 
 type ModalProps = {
@@ -5,27 +6,73 @@ type ModalProps = {
   onClose: () => void
   title: string
   children: React.ReactNode
+  variant?: "box" | "side"
 }
 
-export default function ModalComponent({ isOpen, onClose, title, children }: ModalProps) {
+type modalStyleType = {
+  initial: {}
+  animate: {}
+  classStyle: string
+  containerStyle: string
+}
+
+export default function ModalComponent({ isOpen, onClose, title, children, variant = "box" }: ModalProps) {
+  const sideStyle: modalStyleType = {
+    initial: {
+      opacity: 0,
+      translateX: "40%"
+    },
+    animate: {
+      opacity: 1,
+      translateX: 0
+    },
+    classStyle: "max-w-[618px] h-full w-full p-10",
+    containerStyle: "flex flex-col items-end"
+  }
+  const boxStyle: modalStyleType = {
+    initial: {
+      opacity: 0,
+      scale: 0.5
+    },
+    animate: {
+      opacity: 1,
+      scale: 1
+    },
+    classStyle: "max-w-[605px] w-full p-6 rounded-lg",
+    containerStyle: "flex justify-center items-center"
+  }
+  const style = variant === "box" ? boxStyle : sideStyle
+
   if (!isOpen) return null
 
   return (
-    <div className="fixed inset-0 z-50 p-[50px] bg-[#fefefe] flex flex-col"
+    <div className={`fixed inset-0 z-50 bg-black/30 ${style.containerStyle}`}
       onClick={(e) => e.stopPropagation()}
     >
-      {/* Close button */}
-      <button
-        className="flex items-center text-2xl hover:text-purple-500"
-        onClick={onClose}
-      >
-        <X size={48} className="mr-2" /> {title}
-      </button>
+      <AnimatePresence>
+        <motion.div
+          initial={style.initial}
+          animate={style.animate}
+          exit={style.initial}
+          transition={{ duration: 0.3 }}
+          className={`bg-white ${style.classStyle}`}
+        >
+          <div className="flex justify-between items-center mb-4">
+            <div className="text-xl">{title}</div>
+            <button
+              className="flex items-center text-2xl hover:text-purple-500"
+              onClick={onClose}
+            >
+              <X size={24} />
+            </button>
+          </div>
 
-      {/* Modal Content */}
-      <div className="grow flex justify-center items-center">
-        {children}
-      </div>
+          {/* Modal Content */}
+          <div>
+            {children}
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
